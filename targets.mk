06a1fdda7bbefa728a5a394ecb30071d1dc407d7
Kasper Isager <kasperisager@gmail.com>
2016-10-08 18:33:13 +0200

    Reorganize stuff

---
+++ targets.mk
@@ -0,0 +1,8 @@
+pdf/paper.pdf: \
+  tex/abstract.tex \
+  tex/background.tex \
+  tex/conclusion.tex \
+  tex/evaluation.tex \
+  tex/implementation.tex \
+  tex/introduction.tex \
+  tex/bibliography.bib
